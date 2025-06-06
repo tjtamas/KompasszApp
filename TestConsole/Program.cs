08b77cbe205e794022aa6890e520ce3b1a037bc6
tjtamas <tjtamas@ik.me>
2025-06-07 01:24:24 +0200

    Initial commit: unified repository with KompasszApp and TestConsole

---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using KompasszApp.Models;
+using KompasszApp.Repositories;
+using KompasszApp.Interfaces;
+using KompasszApp.Services;
+using KompasszApp.Infrastructure;
+
+using LiteDB;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = DatabaseFactory.Create();
+            IRepository<Technology> techRepo = new LiteDbRepository<Technology>(db);
+            var seeder = new TechnologySeeder(techRepo);
+
+
+            seeder.Seed();
+            seeder.PrintAll();
+
+
+            Console.WriteLine("\nNyomj Entert a kilépéshez...");
+            Console.ReadLine();
+
+        }
+    }
+}
